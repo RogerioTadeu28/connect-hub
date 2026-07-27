f26411b6a8fb67cb5ac64ddbe3d4daa02ae28aea
Rogerio Tadeu <rogeriosouza73@gmail.com>
2026-07-26 23:31:15 -0300

    Defimos as tabelas User e Movimentacao no schema.prisma; rodamos a migration para criar as tabelas no PostgreSQL

---
+++ connect-hub-backend/prisma/migrations/20260727021426_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movimentacao" (
+    "id" SERIAL NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "valor" DOUBLE PRECISION NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movimentacao_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Movimentacao" ADD CONSTRAINT "Movimentacao_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
